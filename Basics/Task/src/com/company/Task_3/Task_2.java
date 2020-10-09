2367a83d343ce70814b7acce35718aa3ea36109f
ilaylyk <ilay_lyk@mail.ru>
2020-10-09 23:03:24 +0300

    Home Task : Cycles

---
+++ Basics/Task/src/com/company/Task_3/Task_2.java
@@ -0,0 +1,24 @@
+package com.company.Task_3;
+
+import java.util.Scanner;
+
+public class Task_2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.printf("Введите a = ");
+        double a = in.nextDouble();
+        System.out.printf("Введите b = ");
+        double b = in.nextDouble();
+        System.out.printf("Введите h = ");
+        double h = in.nextDouble();
+
+        for (double i = a; i <= b; i += h) {
+            if (i > 2){
+                System.out.println("x = " + i + " y = " + i);
+            }
+            else {
+                System.out.println("x = " + i + " y = " + (-i));
+            }
+        }
+    }
+}
---
+++ Basics/Task/src/com/company/Task_3/Task_5.java
@@ -0,0 +1,13 @@
+package com.company.Task_3;
+
+import java.io.UnsupportedEncodingException;
+
+public class Task_5 {
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        String s = "A"+"a"+"C"+"U"+"o";
+        byte[] b = s.getBytes("US-ASCII");
+        for (int i=0;i<5;i++){
+            System.out.println(b[i]);
+        }
+    }
+}
