70d6c372addd4e18cf85e6a40e2dcfde87c446e9
ilaylyk <ilay_lyk@mail.ru>
2020-10-27 03:07:55 +0300

    Task 10 update

--- Algorithmization/src/com/company/task_1/Task_10.java
+++ Algorithmization/src/com/company/task_1/Task_10.java
@@ -4,10 +4,8 @@ public class Task_10 {
     public static void main(String[] args) {
         int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-        for (int i = 0; i < array.length; i++) {
-            if (i % 2 != 0) {
-                array[i] = 0;
-            }
+        for (int i = 1; i < array.length; i+=2) {
+            array[i] = 0;
         }
         for (int i : array) {
             System.out.print(i + "  ");
